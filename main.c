28ed3ee29a026c89661f6ac05236ccc834f0461a
Lucas DE ALMEIDA SILVA <Lucas DE ALMEIDA SILVA>
2017-06-20 00:12:03 +0200

    Windows + Linux

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
+#ifdef _WIN32
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <tchar.h>
+#include <direct.h>
+#include <io.h>
+
+#define OS_Windows 1
+#define DIV 1048576
+#define WIDTH 7
+#endif
+
+#ifdef linux
+
+#define OS_Windows 0
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#endif
+
 
 int main()
 {
-    printf("Hello world!\n");
+    if(OS_Windows)
+        printf("Hello world from Windows !\n");
+    else
+        printf("Hello World from Linux !\n");
+
+
+
+
     return 0;
 }
